c2e383c117fde8dbff3669b4869bc468f72df7d4
susu1970 <xxx>
2021-08-15 00:43:31 +0800

    mariadb database 'elevencent' -> finished

--- src/db/mariadb/create_elevencent.sql
+++ src/db/mariadb/create_elevencent.sql
@@ -1,170 +1,53 @@
--- MariaDB dump 10.19  Distrib 10.5.10-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: elevencent
--- ------------------------------------------------------
--- Server version	10.5.10-MariaDB-2
+drop database if exists `elevencent`;
+create database `elevencent` default charset=utf8mb4;
+use `elevencent`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+drop table if exists `user_resource`;
+create table `user_resource`(
+`user_resource_id` int unsigned not null,
+`resource_id` int unsigned not null,
+`resource_mask` bigint unsigned not null default 0,
+`create_time` datetime default current_timestamp, 
+`update_time` datetime default current_timestamp on update current_timestamp,
+primary key(`user_resource_id`,`resource_id`)
+)engine=InnoDB;
 
---
--- Table structure for table `account`
---
+drop table if exists `resource`;
+create table `resource`(
+`resource_id` int unsigned not null primary key,
+`resource_mask` bigint unsigned not null,
+`resource_ref` int unsigned not null default 1
+)engine=InnoDB;
 
-DROP TABLE IF EXISTS `account`;	
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `account` (
-  `account_id` bigint(10) unsigned NOT NULL,
-  `resource_id` bigint(20) unsigned NOT NULL DEFAULT 0,
-  `account_mask` bigint(20) unsigned DEFAULT 0,
-  PRIMARY KEY (`account_id`,`resource_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+drop table if exists `name_resource`;
+create table `name_resource`(
+`resource_id` int unsigned not null primary key,
+`name` varchar(255) not null 
+)engine=InnoDB;
 
---
--- Dumping data for table `account`
---
+drop table if exists `file_resource`;
+create table `file_resource`(
+`resource_id` int unsigned not null primary key,
+`file_name` varchar(1026) not null,
+`file_location` varchar(10240) not null 
+)engine=InnoDB;
 
-LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
-UNLOCK TABLES;
+drop table if exists `passwd_resource`;
+create table `passwd_resource`(
+`resource_id` int unsigned not null primary key,
+`resource_mask` bigint unsigned not null default 0,
+`passwd` varchar(4096) default null 
+)engine=InnoDB;
 
---
--- Table structure for table `file_resource`
---
+drop table if exists `post_resource`;
+create table `post_resource`(
+`post_resource_id` int unsigned not null primary key,
+`resource_id` int unsigned,
+`resource_mask` int unsigned not null default 0
+)engine=InnoDB;
 
-DROP TABLE IF EXISTS `file_resource`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `file_resource` (
-  `resource_id` bigint(20) unsigned NOT NULL,
-  `file_name` char(255) DEFAULT NULL,
-  `file_location` varchar(20000) DEFAULT NULL,
-  `comment` text DEFAULT NULL,
-  PRIMARY KEY (`resource_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `file_resource`
---
-
-LOCK TABLES `file_resource` WRITE;
-/*!40000 ALTER TABLE `file_resource` DISABLE KEYS */;
-/*!40000 ALTER TABLE `file_resource` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `friend_resource`
---
-
-DROP TABLE IF EXISTS `friend_resource`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `friend_resource` (
-  `resource_id` bigint(20) unsigned NOT NULL,
-  `friend_mask` bigint(20) unsigned DEFAULT NULL,
-  `friend_account_id` bigint(20) unsigned DEFAULT NULL,
-  `log_id` bigint(20) unsigned DEFAULT NULL,
-  PRIMARY KEY (`resource_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `friend_resource`
---
-
-LOCK TABLES `friend_resource` WRITE;
-/*!40000 ALTER TABLE `friend_resource` DISABLE KEYS */;
-/*!40000 ALTER TABLE `friend_resource` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `log`
---
-
-DROP TABLE IF EXISTS `log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `log` (
-  `log_id` bigint(20) unsigned NOT NULL,
-  `comment` text DEFAULT NULL,
-  PRIMARY KEY (`log_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `log`
---
-
-LOCK TABLES `log` WRITE;
-/*!40000 ALTER TABLE `log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `log` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `nick_name_resource`
---
-
-DROP TABLE IF EXISTS `nick_name_resource`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `nick_name_resource` (
-  `resource_id` bigint(20) unsigned NOT NULL,
-  `nick_name` char(1) DEFAULT NULL,
-  PRIMARY KEY (`resource_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `nick_name_resource`
---
-
-LOCK TABLES `nick_name_resource` WRITE;
-/*!40000 ALTER TABLE `nick_name_resource` DISABLE KEYS */;
-/*!40000 ALTER TABLE `nick_name_resource` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `resource`
---
-
-DROP TABLE IF EXISTS `resource`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `resource` (
-  `resource_id` bigint(20) unsigned NOT NULL,
-  `resource_mask` bigint(20) unsigned DEFAULT 0,
-  PRIMARY KEY (`resource_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `resource`
---
-
-LOCK TABLES `resource` WRITE;
-/*!40000 ALTER TABLE `resource` DISABLE KEYS */;
-/*!40000 ALTER TABLE `resource` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-07-30 19:39:08
+drop table if exists `post_content_resource`;
+create table `post_content_resource`(
+`resource_id` int unsigned not null primary key,
+`post_content` longblob default null 
+)engine=InnoDB;
